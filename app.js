6e97d0fdae445cf2f62f9a5340a5930cc64a53df
brock810 <130688727+brock810@users.noreply.github.com>
2023-06-02 10:50:30 -0500

    another file

--- app.js
+++ app.js
@@ -4,7 +4,32 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Brock";
+let LastName= "Stanley";
+var age= 22;
 
 // Exercise 2
+lastName = "Stanley";
+age = 22;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear= 1995;
+let isCaseSensitive= true;
+
+console.log('${language} was first release in ${createdYear}.');
+
+let product = "T-shirty"
+let price = 19.99;
+let isOnSale= false;
+let salePercentage= 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
